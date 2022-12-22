0c8ff48d054892dd4297291df6ad829343f3923d
ebraekke <ebraekke@yahoo.com>
2022-12-22 16:03:09 +0100

    fine tuning of outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,18 @@
 
+/* uncomment for debugging -- not needed otherwise
 output "ads" {
   value = data.oci_identity_availability_domains.ads
 }
 
 output "vcn_name" {
   value = data.oci_core_vcn.this_vcn.dns_label
+}
+*/
+
+output "worker_private_ips" {
+  value = module.worker.worker_private_ips
+}
+
+output "worker_ocid_list" {
+  value = module.worker.worker_ocid_list
 }
